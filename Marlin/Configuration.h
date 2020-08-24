8d947a200f4b70b8deea6ec828d85f2b846ab601
PaschyS <7069603+PaschyS@users.noreply.github.com>
2020-08-25 01:40:17 +0200

    Set external EEPROM

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1467,11 +1467,13 @@
  *   M501 - Read settings from EEPROM. (i.e., Throw away unsaved changes)
  *   M502 - Revert settings to "factory" defaults. (Follow with M500 to init the EEPROM.)
  */
-//#define EEPROM_SETTINGS     // Persistent storage with M500 and M501
+#define EEPROM_SETTINGS     // Persistent storage with M500 and M501
 //#define DISABLE_M503        // Saves ~2700 bytes of PROGMEM. Disable for release!
 #define EEPROM_CHITCHAT       // Give feedback on EEPROM commands. Disable to save PROGMEM.
 #define EEPROM_BOOT_SILENT    // Keep M503 quiet and only give errors during first load
 #if ENABLED(EEPROM_SETTINGS)
+  #define I2C_EEPROM //I2C external eeprom module AT24C256
+  //#define FLASH_EEPROM_EMULATION
   //#define EEPROM_AUTO_INIT  // Init EEPROM automatically on any errors.
 #endif
 
