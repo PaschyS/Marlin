c9840aa7a4dcd5d99aea20242c54d71ba5d37dfd
PaschyS <7069603+PaschyS@users.noreply.github.com>
2020-09-23 01:31:12 +0200

    add Pin for z_min_probe with BLTouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -877,7 +877,7 @@
  *      - normally-open switches to 5V and D32.
  *
  */
-#define Z_MIN_PROBE_PIN // Pin 32 is the RAMPS default
+#define Z_MIN_PROBE_PIN P0_10 // SKR1.4Turbo pin BLtouch
 
 /**
  * Probe Type
